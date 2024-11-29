b40cc70139c04002df50478858551c9bc37be0c3
Ely Rose Bosangit <bosangitelyrose05@gmail.com>
2024-11-29 10:11:06 +0800

    add App css

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 
+import '../css/app.css';
+
 class App extends React.Component {
   render() {
 	return (
 		<>
 			<NavigationBar active='app' />
 	  <div>
-		<h1>App</h1>
+				<h1 id="appname">App</h1>
 	  </div>
 		</>
 	);
