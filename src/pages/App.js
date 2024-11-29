837be64902d07b9fc33700ca484b3eb6c22bcfb3
Ely Rose Bosangit <bosangitelyrose05@gmail.com>
2024-11-29 10:03:50 +0800

    add navigationbar to App page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import NavigationBar from "../components/NavigationBar";
 
 class App extends React.Component {
   render() {
 	return (
+		<>
+			<NavigationBar active='app' />
 	  <div>
 		<h1>App</h1>
 	  </div>
+		</>
 	);
   }
 }
