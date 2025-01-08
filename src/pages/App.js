b25230f5d480146cba6ab0049a8527179dbe23d6
Daniel John Baynosa <firedantsm@gmail.com>
2025-01-08 12:44:08 +0800

    Refactor download section layout and update download button functionality

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,56 +5,63 @@ import Button from "../components/Button";
 import BRKPAW from "../assets/BRKPAW.png";
 import mobile from "../assets/mobileapp.png";
 
+import { Link } from "react-router-dom";
+
 import "../css/app.css";
 
 class App extends React.Component {
-  render() {
-    return (
-      <>
-        <NavigationBar active="app" />
+	render() {
+		return (
+			<>
+				<NavigationBar active="app" />
 
-        <img src={BRKPAW} alt="brkpaw" id="pawprint" />
-        <img src={mobile} alt="mobileapp" id="cellphone" />
-        <section id="downloadapp">
-          <img src={BRKPAW} alt="brkpaw" id="pawprint" />
-          <img src={mobile} alt="mobileapp" id="cellphone" />
-          <div>
-            <h1 id="appname">
-              <span>Download Our</span>
-              <br />
-              Compawnion App
-            </h1>
+				<section id="downloadapp">
+					<div>
+						<img src={BRKPAW} alt="brkpaw" id="pawprint" />
+						<img src={mobile} alt="mobileapp" id="cellphone" />
+					</div>
+					<div>
+						<h1>
+							<span>Download Our</span>
+							<br />
+							Compawnion App
+						</h1>
 
-            <p id="description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae
-              venenatis elit. Mauris eu porta diam. Cras congue tempus tempor.
-              Donec aliquet turpis ac arcu efficitur, ut dictum sem rhoncus.
-              Nunc velit turpis, tincidunt eu posuere vel, blandit id magna.
-              Maecenas vulputate molestie eros, nec pulvinar justo eleifend vel.
-              Vestibulum.
-              <br />
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae
-              venenatis elit. Mauris eu porta diam. Cras congue tempus tempor.
-              Donec aliquet turpis ac arcu efficitur, ut dictum sem rhoncus.
-              Nunc velit turpis, tincidunt eu posuere vel, blandit id magna.
-              Maecenas vulputate molestie eros, nec pulvinar justo eleifend vel.
-              Vestibulum.
-            </p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae
+							venenatis elit. Mauris eu porta diam. Cras congue tempus tempor.
+							Donec aliquet turpis ac arcu efficitur, ut dictum sem rhoncus.
+							Nunc velit turpis, tincidunt eu posuere vel, blandit id magna.
+							Maecenas vulputate molestie eros, nec pulvinar justo eleifend vel.
+							Vestibulum.
+							<br />
+							<br />
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae
+							venenatis elit. Mauris eu porta diam. Cras congue tempus tempor.
+							Donec aliquet turpis ac arcu efficitur, ut dictum sem rhoncus.
+							Nunc velit turpis, tincidunt eu posuere vel, blandit id magna.
+							Maecenas vulputate molestie eros, nec pulvinar justo eleifend vel.
+							Vestibulum.
+						</p>
 
-            <Button
-				href="https://github.com/Barkcode-Compawnion/compawnion-app/releases/download/v.1.0.0/Compawnion.apk"
-              	title="download"
-              	id="dlbtn"
-            >
-              Download App
-            </Button>
-          </div>
-        </section>
-        <Footer active="home" />
-      </>
-    );
-  }
-}
+							<Button
+								title="download"
+
+								onClick={() => {
+									const Link = document.createElement("a");
+									Link.href = "/Compawnion.apk";
+									Link.download = "Compawnion.apk";
+									Link.click();
+								}}
+							>
+								Download App
+							</Button>
+						</div>
+					</section>
+					<Footer active="home" />
+				</>
+			);
+		}
+	}
 
 export default App;
